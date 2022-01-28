9329e8d34108fe349df6995e0b8d8b470eed2880
umarhunter <umor.uddin54@myhunter.cuny.edu>
2022-01-28 01:45:07 -0500

    added mathematical logic and finished the essential component of program.

--- PythonPasswordChanger/main.py
+++ PythonPasswordChanger/main.py
@@ -1,33 +1,61 @@
 import getpass
 import random
 
-def convert2ascii(string):
-    return " ".join(str(ord(char)) for char in string)
+def convert2ascii(string, num):
+    return " ".join(str(ord(char) + num) for char in string)
+
 
 def convert2string(number):
     return chr(number)
 
+
+def randomizer(param):
+    if param == 1:
+        return random.randint(1,4)
+    elif param == 2:
+        return random.randint(4, 8)
+    else:
+        return random.randint(9, 18)
+
+
 def createsmall():
-    print("DEBUG: createsmall function launched")
+    #print("DEBUG: createsmall function launched")
     foo = input("Please enter the password you want to encrypt: ")
-    newfoo = convert2ascii(foo)
-    print("DEBUG: convert2ascii returned: ", newfoo)
+    newfoo = convert2ascii(foo, randomizer(1))
+    #print("DEBUG: convert2ascii returned: ", newfoo)
     splitfoo = newfoo.split(" ")
-    int_list = [int(x) for x in splitfoo]
-    print(int_list)
-    for i in range(len(int_list)):
-        listvar = convert2string(int_list[i])
+    integer_list = [int(x) for x in splitfoo]
+    #print("DEBUG: int_list: ", integer_list)
+    appendvar = []
+    for i in range(len(integer_list)):
+        appendvar.append(str(convert2string(integer_list[i])))
+    print("Your new password is: ", "".join(appendvar))
+
 def createmedium():
-    print("DEBUG: createmedium function launched")
+    # print("DEBUG: createsmall function launched")
     foo = input("Please enter the password you want to encrypt: ")
-    newfoo = convert2ascii(foo)
-    print("DEBUG: convert2ascii returned: ", newfoo)
+    newfoo = convert2ascii(foo, randomizer(2))
+    # print("DEBUG: convert2ascii returned: ", newfoo)
+    splitfoo = newfoo.split(" ")
+    integer_list = [int(x) for x in splitfoo]
+    # print("DEBUG: int_list: ", integer_list)
+    appendvar = []
+    for i in range(len(integer_list)):
+        appendvar.append(str(convert2string(integer_list[i])))
+    print("Your new password is: ", "".join(appendvar))
 
 def createlarge():
-    print("DEBUG: createlarge function launched")
+    # print("DEBUG: createsmall function launched")
     foo = input("Please enter the password you want to encrypt: ")
-    newfoo = convert2ascii(foo)
-    print("DEBUG: convert2ascii returned: ", newfoo)
+    newfoo = convert2ascii(foo, randomizer(3))
+    # print("DEBUG: convert2ascii returned: ", newfoo)
+    splitfoo = newfoo.split(" ")
+    integer_list = [int(x) for x in splitfoo]
+    # print("DEBUG: int_list: ", integer_list)
+    appendvar = []
+    for i in range(len(integer_list)):
+        appendvar.append(str(convert2string(integer_list[i])))
+    print("Your new password is: ", "".join(appendvar))
 
 def createformulas():
     print("DEBUG: createformulas function launched")
@@ -54,13 +82,20 @@ def passwordfunction():
 
 if __name__ == '__main__':
     username = getpass.getuser()
-    print("Python Password Manager V1")
+    print("Python Password Manager V2")
     print("..........................")
-    info = open('information.txt', 'w+')
+    print("WARNING: AS OF NOW, ONLY PRE-DETERMINED FORMULAS ARE WORKING")
+    #info = open('information.txt', 'w+') // keeping in case of future implementation with file systems
 
     print("Welcome, ", username)
-    formulas = input("Will you require pre-determined formulas? y/n:")
-    if formulas == "y":
-        createformulas()
-    elif formulas == "n":
-        passwordfunction()
+    increment = 1
+    while(increment > 0):
+        formulas = input("Will you require pre-determined formulas? y/n:")
+        if formulas == "y":
+            increment = -1
+            createformulas()
+        elif formulas == "n":
+            increment = -1
+            passwordfunction()
+        else:
+            print("Please enter a valid input: (y/n)")
